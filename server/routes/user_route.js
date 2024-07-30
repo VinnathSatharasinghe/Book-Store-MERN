db5509ba5790161c6e9b71445c887ee09987f763
vinnath <vinnathninura@gmail.com>
2024-07-30 14:29:28 +0530

    modify user-route

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -1,100 +1,3 @@
-// const router = require("express").Router();
-// const bycript = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-
-// const User = require("../models/user");
-// const authenticateUser = require("../auth/userAuth");
-
-
-
-// router.post("/signup", async (req, res) => {
-//   try {
-//     const { username, password, email, address } = req.body;
-
-//     // if (username.length < 4) {
-//     //     return res.status(400).json({ message: "Username must be at least 4 characters long" });
-//     // }
-
-//     // if (password.length < 5) {
-//     //     return res.status(400).json({ message: "Password must be at least 5 characters long" });
-//     // }
-
-//     const hashedPassword = bycript.hashSync(password, 10);
-
-//     const userExists = await User.findOne({ username: username });
-//     if (userExists) {
-//       return res.status(400).json({ message: "Username already exists" });
-//     }
-
-//     const emailExists = await User.findOne({ email: email });
-//     if (emailExists) {
-//       return res.status(400).json({ message: "Email already exists" });
-//     }
-
-//     const newUser = new User({
-//       username: username,
-//       password: hashedPassword,
-//       email: email,
-//       address: address,
-//     });
-
-//     await newUser.save();
-//     return res.status(200).json({ message: "User saved successfully" });
-//   } catch {
-//     res.status(404).json({ message: "INTERNAL ERROR" });
-//   }
-// });
-
-//login
-
-//     const { username, password } = req.body;
-//     const user = await User.findOne({ username: username });
-//     if (!user) {
-//         return res.status(400).json({ message: "Invalid username" });
-//     }
-//     const isMatch = bycript.compareSync(password, user.password);
-//     if (!isMatch) {
-//         return res.status(400).json({ message: "Invalid password" });
-//     }
-//     const token = user.generateAuthToken();
-//     const decodedToken = jwt.decode(token);
-//     const expiresAt = new Date(decodedToken.exp * 1000); // Convert to milliseconds
-//     return res.status(200).json({
-//         message: "Logged in successfully",
-//         token,
-//         expiresAt
-//     });
-
-// router.post("/login", async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-
-//     const authResult = await authenticateUser(username, password);
-
-//     if (authResult.token) {
-
-//       return res.status(200).json(authResult);
-//     } else {
-
-
-//       if (authResult.message === "Token expired") {
-//         return res
-//           .status(401)
-//           .json({ message: "Token expired. Please log in again." });
-//       } else {
-//         return res.status(403).json({ message: authResult.message });
-//       }
-//     }
-//   } catch (error) {
-//     res.status(404).json({ message: "Server Error" });
-//     console.error(error);
-//   }
-// });
-
-
-// module.exports = router;
-
-
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -135,6 +38,7 @@ router.post("/signup", async (req, res) => {
     const hashedPassword = bcrypt.hashSync(password, 10);
 
     const userExists = await User.findOne({ username: username });
+    
     if (userExists) {
       return res.status(200).json({ message: "caseuser" });
     }
