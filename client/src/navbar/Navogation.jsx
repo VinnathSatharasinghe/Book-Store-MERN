8c5b66a7e637b9dc527700dde8c7dff9164634e8
vinnath <vinnathninura@gmail.com>
2024-08-18 13:24:36 +0530

    qq

--- client/src/navbar/Navogation.jsx
+++ client/src/navbar/Navogation.jsx
@@ -1,7 +1,7 @@
 
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import "/navscss.css";
+import "/client/src/navbar/navscss.css";
 
 const Navigation = () => {
   return (
