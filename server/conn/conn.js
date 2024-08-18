683cbf292f4949557db5aa96eef81628feffbcd2
vinnath <vinnathninura@gmail.com>
2024-08-19 00:33:41 +0530

    add config files to server

--- server/conn/conn.js
+++ server/conn/conn.js
@@ -1,11 +1,30 @@
+// const mongoose = require('mongoose');
+
+// const conn = async () => {
+//     try {
+//         await mongoose.connect(
+//             "mongodb+srv://vinnath:ggvinnath@books2024.wxxqwzu.mongodb.net/?retryWrites=true&w=majority&appName=books2024",
+//             {
+              
+//             }
+//         );
+//         console.log("Connected to MongoDB");
+//     } catch (err) {
+//         console.error("Error connecting to MongoDB", err);
+//     }
+// };
+
+// conn();
+
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const conn = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://vinnath:ggvinnath@books2024.wxxqwzu.mongodb.net/?retryWrites=true&w=majority&appName=books2024",
+            process.env.MONGODB_URI,
             {
-              
+                //  additional options 
             }
         );
         console.log("Connected to MongoDB");
@@ -15,5 +34,6 @@ const conn = async () => {
 };
 
 conn();
+
 
 
