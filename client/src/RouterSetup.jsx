a7fb68576b864e69f636764b8e6de8bf5e55cd6a
vinnath <vinnathninura@gmail.com>
2024-08-18 14:17:41 +0530

    asasaw

--- client/src/RouterSetup.jsx
+++ client/src/RouterSetup.jsx
@@ -9,24 +9,24 @@ import ALogin from "./pages/Adminlogin.jsx";
 import Signup from "./pages/Signup.jsx";
 
 
-// import Book from "./pages/Admin_afterlogin/Add_book";
-// import ASignup from "./pages/Admin_signup";
-// import Uupdate from "./pages/User_afterlogin/Update_user";
-// import Aupdate from "./pages/Admin_afterlogin/Update_admin.jsx";
-// import Userafterlogin from "./pages/User_afterlogin/User-afterlogin";
-// import Vbook from "./pages/Admin_afterlogin/BooksTable";
-// import Alluser from "./pages/Admin_afterlogin/All_users";
-// import Bookupdate from "./pages/Admin_afterlogin/Bookupdate";
-// import Adminafterlogin from "./pages/Admin_afterlogin/Admin-afterlog";
-// import Allbooks from "./pages/Books";
-// import AllAdmins from "./pages/Admin_afterlogin/All_admins";
+import Book from "./pages/Admin_afterlogin/Add_book";
+import ASignup from "./pages/Admin_signup";
+import Uupdate from "./pages/User_afterlogin/Update_user";
+import Aupdate from "./pages/Admin_afterlogin/Update_admin.jsx";
+import Userafterlogin from "./pages/User_afterlogin/User-afterlogin";
+import Vbook from "./pages/Admin_afterlogin/BooksTable";
+import Alluser from "./pages/Admin_afterlogin/All_users";
+import Bookupdate from "./pages/Admin_afterlogin/Bookupdate";
+import Adminafterlogin from "./pages/Admin_afterlogin/Admin-afterlog";
+import Allbooks from "./pages/Books";
+import AllAdmins from "./pages/Admin_afterlogin/All_admins";
 
 //testing
-// import UUpdate from "./redux/Test/Updateuser.jsx";
-// import Tlogin from "./redux/Test/testlogin.jsx";
+import UUpdate from "./redux/Test/Updateuser.jsx";
+import Tlogin from "./redux/Test/testlogin.jsx";
 
 //ProtectedRoute
-// import NewProtectedRoute from "./redux/routes/Testprotect";
+import NewProtectedRoute from "./redux/routes/Testprotect";
 
 const router = createBrowserRouter([
   {
@@ -49,62 +49,62 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <Signup />,
    },
-  // {
-  //   path: "/asignup",
-  //   element: <ASignup />,
-  // },
-  // // {
-  // //   path: "/uupdate",
-  // //   element: <Uupdate />,
-  // // },
-  // {
-  //   path: "/aupdate",
-  //   element: <Aupdate />,
-  // },
-  // {
-  //   path: "/uafterlogin",
-  //   element: (
-  //     <NewProtectedRoute>
-  //       <Userafterlogin />
-  //     </NewProtectedRoute>
-  //   ),
-  // },
-  // {
-  //   path: "/aafterlogin",
-  //   element: <Adminafterlogin />,
-  // },
-  // {
-  //   path: "/alluser",
-  //   element: <Alluser />,
-  // },
-  // {
-  //   path: "/alladmins",
-  //   element: <AllAdmins />,
-  // },
-  // {
-  //   path: "/addbook",
-  //   element: <Book />,
-  // },
-  // {
-  //   path: "/vbooks",
-  //   element: <Vbook />,
-  // },
-  // {
-  //   path: "/bupdate",
-  //   element: <Bookupdate />,
-  // },
-  // {
-  //   path: "/allbooks",
-  //   element: <Allbooks />,
-  // },
-  // {
-  //   path: "/tuu",
-  //   element: <UUpdate />,
-  // },
-  // {
-  //   path: "/tl",
-  //   element: <Tlogin />,
-  // },
+  {
+    path: "/asignup",
+    element: <ASignup />,
+  },
+  {
+    path: "/uupdate",
+    element: <Uupdate />,
+  },
+  {
+    path: "/aupdate",
+    element: <Aupdate />,
+  },
+  {
+    path: "/uafterlogin",
+    element: (
+      <NewProtectedRoute>
+        <Userafterlogin />
+      </NewProtectedRoute>
+    ),
+  },
+  {
+    path: "/aafterlogin",
+    element: <Adminafterlogin />,
+  },
+  {
+    path: "/alluser",
+    element: <Alluser />,
+  },
+  {
+    path: "/alladmins",
+    element: <AllAdmins />,
+  },
+  {
+    path: "/addbook",
+    element: <Book />,
+  },
+  {
+    path: "/vbooks",
+    element: <Vbook />,
+  },
+  {
+    path: "/bupdate",
+    element: <Bookupdate />,
+  },
+  {
+    path: "/allbooks",
+    element: <Allbooks />,
+  },
+  {
+    path: "/tuu",
+    element: <UUpdate />,
+  },
+  {
+    path: "/tl",
+    element: <Tlogin />,
+  },
 ]);
 
 const RouterSetup = () => (
