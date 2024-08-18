dcb16b7799922dddf358ddd41baf902eb115693f
vinnath <vinnathninura@gmail.com>
2024-08-18 13:35:16 +0530

    aq

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import RouterSetup from "./RouterSetup";
 import StarsCanvas from "./components/Animation/StarsCanvas";
-import NavBar from "./navbar/Navigation";
 
 function App() {
   return (
     <>
       <StarsCanvas />
       <div style={{ position: "relative", zIndex: 1 }}>
-        <NavBar />
         <RouterSetup />
       </div>
     </>
