bf6c3eb8a51c7138dc937c727167ce5dceaa8b79
vinnath <vinnathninura@gmail.com>
2024-08-18 13:56:43 +0530

    aada

--- client/src/pages/admin_login.jsx
+++ client/src/pages/admin_login.jsx
@@ -1,145 +1,19 @@
-// import Button from "react-bootstrap/Button";
-// import Form from "react-bootstrap/Form";
-// import { useState } from "react";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-// import { ToastContainer, toast } from "react-toastify";
-// import "../css/log.css"; 
-// import { useAuth } from "../components/Auth/AuthContext.tsx";
-// import "react-toastify/dist/ReactToastify.css";
-
-// function Admin_login() {
-//   const [username, setUsername] = useState();
-//   const [password, setPassword] = useState();
-//   const navigate = useNavigate();
-//   const { alogin } = useAuth();
-
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     axios
-//       .post("http://localhost:5000/api/admin-login", { username, password })
-//       .then((result) => {
-//         console.log(result);
-
-//         if (result.data.message == "noadmin") {
-//           toast.error("Login Failed. No user.");
-
-//         } else if (result.data.message == "caseadmin") {
-//           toast.error("Login Failed. No user existed.");
-
-//         } else if (result.data.message == "nopass") {
-//           toast.error("Login Failed. No Password");
-          
-//         } else if (result.data.message == "casepass") {
-//           toast.error("Login Failed. Incorrect Password");
-
-//         } else if (result.data.message == "adminloginok") {
-//           toast.success("Admin Login Successful!");
-
-//           const token = result.data.token; 
-//           const expiresAt = result.data.expiresAt;
-
-//           localStorage.setItem("token", token);
-//           localStorage.setItem("tokenExpiration", expiresAt);
-//           alogin(token, expiresAt);
-  
-
-//           setTimeout(() => {
-//             navigate("/aafterlogin", {
-//               state: {
-//                  id: result.data.id,
-//                  name: result.data.name,
-//               },
-//             });
-//           },1000);
-       
-//         } else {
-//           toast.error("Admin Login Failed. No record existed.");
-//         }
-//       })
-//       .catch((err) => console.log(err));
-//   };
-
-//   return (
-//     <div>
-//       <div className="mainx">
-//         <div className="formx">
-//           <Form onSubmit={handleSubmit}>
-//           <h2 style={{marginTop:'50px'}}>Admin Login</h2>
-//             <Form.Group className="mb-3" controlId="formBasicUsername">
-//               <Form.Label>Username</Form.Label>
-//               <br />
-//               <input
-//                 type="text"
-//                 name="admin-username"
-//                 placeholder="Enter Username"
-//                 autoComplete="off"
-//                 defaultValue={""}
-//                 onChange={(e) => setUsername(e.target.value)}
-//               />
-//             </Form.Group>
-//             <br />
-//             <Form.Group className="mb-3" controlId="formBasicPassword">
-//               <Form.Label>Password</Form.Label>
-//               <br />
-//               <input
-//                 type="password"
-//                 name="admin-password"
-//                 placeholder="Enter Password"
-//                 autoComplete="off"
-//                 defaultValue={""}
-//                 onChange={(e) => setPassword(e.target.value)}
-//               />
-//             </Form.Group>
-
-//             <Button variant="primary" type="submit">
-//               Login
-//             </Button>
-
-//             <ToastContainer
-//               position="top-right"
-//               autoClose={5000}
-//               hideProgressBar={false}
-//               newestOnTop={false}
-//               closeOnClick
-//               rtl={false}
-//               pauseOnFocusLoss
-//               draggable
-//               pauseOnHover
-//               theme="dark"
-//             />
-//           </Form>
-
-//           <Button variant="primary" type="login">
-//             <a href="/">Home</a>
-//           </Button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Admin_login;
-
-
-
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import "../css/log.css";
-import { useAuth } from "../components/Auth/AuthContext";
+import "../css/log.css"; 
+import { useAuth } from "../components/Auth/AuthContext.tsx";
 import "react-toastify/dist/ReactToastify.css";
 
 function Admin_login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
   const navigate = useNavigate();
   const { alogin } = useAuth();
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -149,42 +23,40 @@ function Admin_login() {
       .then((result) => {
         console.log(result);
 
-        const message = result.data.message;
+        if (result.data.message == "noadmin") {
+          toast.error("Login Failed. No user.");
 
-        switch (message) {
-          case "noadmin":
-            toast.error("Login Failed. No user.");
-            break;
-          case "caseadmin":
-            toast.error("Login Failed. No user existed.");
-            break;
-          case "nopass":
-            toast.error("Login Failed. No Password");
-            break;
-          case "casepass":
-            toast.error("Login Failed. Incorrect Password");
-            break;
-          case "adminloginok":
-            toast.success("Admin Login Successful!");
+        } else if (result.data.message == "caseadmin") {
+          toast.error("Login Failed. No user existed.");
 
-            const token = result.data.token;
-            const expiresAt = result.data.expiresAt;
+        } else if (result.data.message == "nopass") {
+          toast.error("Login Failed. No Password");
+          
+        } else if (result.data.message == "casepass") {
+          toast.error("Login Failed. Incorrect Password");
 
-            localStorage.setItem("token", token);
-            localStorage.setItem("tokenExpiration", expiresAt);
-            alogin(token, expiresAt);
+        } else if (result.data.message == "adminloginok") {
+          toast.success("Admin Login Successful!");
 
-            setTimeout(() => {
-              navigate("/aafterlogin", {
-                state: {
-                  id: result.data.id,
-                  name: result.data.name,
-                },
-              });
-            }, 1000);
-            break;
-          default:
-            toast.error("Admin Login Failed. No record existed.");
+          const token = result.data.token; 
+          const expiresAt = result.data.expiresAt;
+
+          localStorage.setItem("token", token);
+          localStorage.setItem("tokenExpiration", expiresAt);
+          alogin(token, expiresAt);
+  
+
+          setTimeout(() => {
+            navigate("/aafterlogin", {
+              state: {
+                 id: result.data.id,
+                 name: result.data.name,
+              },
+            });
+          },1000);
+       
+        } else {
+          toast.error("Admin Login Failed. No record existed.");
         }
       })
       .catch((err) => console.log(err));
@@ -195,26 +67,29 @@ function Admin_login() {
       <div className="mainx">
         <div className="formx">
           <Form onSubmit={handleSubmit}>
-            <h2 style={{ marginTop: "50px" }}>Admin Login</h2>
+          <h2 style={{marginTop:'50px'}}>Admin Login</h2>
             <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
+              <br />
               <input
                 type="text"
                 name="admin-username"
                 placeholder="Enter Username"
                 autoComplete="off"
-                value={username}
+                defaultValue={""}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </Form.Group>
+            <br />
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
+              <br />
               <input
                 type="password"
                 name="admin-password"
                 placeholder="Enter Password"
                 autoComplete="off"
-                value={password}
+                defaultValue={""}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </Form.Group>
@@ -237,8 +112,8 @@ function Admin_login() {
             />
           </Form>
 
-          <Button variant="primary" href="/">
-            Home
+          <Button variant="primary" type="login">
+            <a href="/">Home</a>
           </Button>
         </div>
       </div>
@@ -247,6 +122,5 @@ function Admin_login() {
 }
 
 export default Admin_login;
-
 
 
